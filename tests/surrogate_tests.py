b98d00a516dfa9e98723686229ebe1f39b524388
skruger <shaun.kruger@gmail.com>
2013-02-28 23:09:33 -0700

    Add test get against httpbin.org to proxy test.

--- tests/surrogate_tests.py
+++ tests/surrogate_tests.py
@@ -1,11 +1,21 @@
 import unittest
 import requests
+import json
 
 proxies = { "http": "http://localhost:3129",
             "https": "http://localhost:3129"}
 
 class TestSurrogate(unittest.TestCase):
 
+    def test_get(self):
+        rp = requests.get("http://httpbin.org/get", proxies=proxies)
+        r = requests.get("http://httpbin.org/get")
+        self.assertEqual(rp.status_code, requests.codes.ok)
+        self.assertEqual(r.status_code, requests.codes.ok)
+        jsnp = json.loads(rp.content)
+        jsn = json.loads(r.content)
+        self.assertEqual(jsnp['url'], jsn['url'])
+        
     
     def test_get_google(self):
         r = requests.get("https://www.google.com", proxies=proxies)
